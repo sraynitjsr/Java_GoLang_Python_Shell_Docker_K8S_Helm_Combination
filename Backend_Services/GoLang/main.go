222ad87346f3d2c56ccd1b74588594656e016e63
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-05-14 20:12:15 +0530

    GoLang Mux REST API

--- Backend_Services/GoLang/main.go
+++ Backend_Services/GoLang/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Println("This Folder Will Have GoLang Backend Service - Service Number 1")
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello From GoLang API")
 }
